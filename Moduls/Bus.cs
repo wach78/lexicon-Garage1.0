c9c1136abedb75c227201e64bd7aec24a91664c6
yggdevsec <dev@yggdevsec.com>
2026-05-07 15:40:00 +0200

    added class bus

---
+++ Moduls/Bus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GarageV1.Moduls
+{
+    internal class Bus: Vehicle
+    {
+        public Bus(string numberPlate, string color, int numberOfWheels, int numberOfSeats)
+        : base(numberPlate, color, numberOfWheels)
+        {
+            NumberOfSeats = numberOfSeats;
+        }
+
+        public int NumberOfSeats { get;}
+    }
+}
