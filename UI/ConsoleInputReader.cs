23d8cb5a86c67650e1246d1543be7bf907bf3f72
yggdevsec <dev@yggdevsec.com>
2026-05-07 21:14:47 +0200

    consoleInputReader

---
+++ UI/ConsoleInputReader.cs
@@ -0,0 +1,36 @@
+﻿using GarageV1.Enums;
+
+namespace GarageV1.UI
+{
+    internal static class ConsoleInputReader
+    {
+        public static MenuChoice? ReadMainMenuChoice()
+        {
+     
+            string? input = Console.ReadLine();
+
+            if (
+                int.TryParse(input, out int numericChoice)
+                && Enum.IsDefined(typeof(MenuChoice), numericChoice)
+            )
+            {
+                return (MenuChoice)numericChoice;
+            }
+
+            return null;
+         }
+        
+
+        public static int? ReadPositiveInt()
+        {
+            string? input = Console.ReadLine();
+
+            if (int.TryParse(input, out int value) && value > 0)
+            {
+                return value;
+            }
+
+            return null;
+        }
+    }
+}
