cafe7f1123dfbd0664b85c4b0828aedeb3da35a2
yggdevsec <dev@yggdevsec.com>
2026-05-07 16:08:56 +0200

    added enum MenuChoice

---
+++ Enums/MenuChoice.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace GarageV1.Enums
+{
+    enum MenuChoice
+    {
+        Exit = 0,
+        CreateGarage = 1,
+        PopulateGarage = 2,
+        ListParkedVehicles = 3,
+        ListVehicleTypes = 4,
+        ParkVehicle = 5,
+        RemoveVehicle = 6,
+        FindVehicleByRegistrationNumber = 7,
+        SearchVehicles = 8
+    }
+}
