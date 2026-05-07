b72f7dd17fbf8d26fad69e2b9abfc4d08901f579
yggdevsec <dev@yggdevsec.com>
2026-05-07 15:34:51 +0200

    added class MotorCycle

---
+++ Moduls/MotorCycle.cs
@@ -0,0 +1,17 @@
+﻿using GarageV1.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GarageV1.Moduls
+{
+    internal class MotorCycle: Vehicle
+    {
+        public MotorCycle(string numberPlate, string color, int numberOfWheels, int cylinderVolume)
+        : base(numberPlate, color, numberOfWheels)
+        {
+            CylinderVolume = cylinderVolume;
+        }
+        public int CylinderVolume { get;}
+    }
+}
