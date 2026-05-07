d9f4a61e90f40eb259247ee0d56f7ebe2f656a02
yggdevsec <dev@yggdevsec.com>
2026-05-07 15:43:16 +0200

    added class airplane

---
+++ Moduls/AirPlane.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GarageV1.Moduls
+{
+    internal class AirPlane: Vehicle
+    {
+        public AirPlane(string numberPlate, string color, int numberOfWheels, int numberOfEngines)
+        : base(numberPlate, color, numberOfWheels)
+        {
+            NumberOfEngines = numberOfEngines;
+        }
+        public int NumberOfEngines { get;}
+    }
+}
