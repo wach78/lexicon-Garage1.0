8c3592710bb009f001ecad44caf8ca9ac1025e09
yggdevsec <dev@yggdevsec.com>
2026-05-07 15:51:50 +0200

    added class Garage

---
+++ Moduls/Garage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GarageV1.Moduls
+{
+    internal class Garage
+    {
+        private readonly Vehicle[] parkedVehicles;
+        public Garage(int capacity)
+        {
+            Capacity = capacity;
+            parkedVehicles = new Vehicle[capacity];
+        }
+
+        public int Capacity { get; }
+    }
+}
